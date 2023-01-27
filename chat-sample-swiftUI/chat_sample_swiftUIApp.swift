c6eab974083b73a553036caf2835d2148ce4ad15
Yuta Shoji <shoshoshon.music@gmail.com>
2023-01-27 11:48:02 +0900

    Initial Commit

---
+++ chat-sample-swiftUI/chat_sample_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_sample_swiftUIApp.swift
+//  chat-sample-swiftUI
+//
+//  Created by Yuta Shoji on 2023/01/27.
+//
+
+import SwiftUI
+
+@main
+struct chat_sample_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
