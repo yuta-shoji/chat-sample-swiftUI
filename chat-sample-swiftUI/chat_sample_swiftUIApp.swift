55bcc6332631c4ee7a31d1c956d4df37223310ab
Shion Koga <shion_koga@mail.toyota.co.jp>
2023-02-01 20:01:09 +0900

    2/1分
    Co-authored-by: Yuta Shoji <yuta_shoji@mail.toyota.co.jp>

--- chat-sample-swiftUI/chat_sample_swiftUIApp.swift
+++ chat-sample-swiftUI/chat_sample_swiftUIApp.swift
@@ -2,11 +2,14 @@ import SwiftUI
 import Amplify
 import AWSDataStorePlugin
 import AWSAPIPlugin
+import UserNotifications
+import NCMB
 
 @main
 struct chat_sample_swiftUIApp: App {
     @StateObject private var modelData = ModelData()
     
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -28,8 +31,78 @@ struct chat_sample_swiftUIApp: App {
             try Amplify.configure()
             print("Initialized Amplify");
         } catch {
-            // simplified error handling for the tutorial
             print("Could not initialize Amplify: \(error)")
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self
+
+          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+          UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        } else {
+          let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+          application.registerUserNotificationSettings(settings)
+        }
+
+        application.registerForRemoteNotifications()
+        return true
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+       print(error.localizedDescription)
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        //端末情報を扱うNCMBInstallationのインスタンスを作成
+        let installation : NCMBInstallation = NCMBInstallation.currentInstallation
+
+        //Device Tokenを設定
+        installation.setDeviceTokenFromData(data: deviceToken)
+
+        //端末情報をデータストアに登録
+        installation.saveInBackground { result in
+            switch result {
+            case .success:
+                //端末情報の登録が成功した場合の処理
+                print("保存に成功しました")
+            case let .failure(error):
+                //端末情報の登録が失敗した場合の処理
+                print("保存に失敗しました: \(error)")
+                return;
+            }
+        }
+    }
+}
+
+extension AppDelegate : UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+
+        print(userInfo)
+
+        // Change this to your preferred presentation option
+        completionHandler([[.banner, .badge, .sound]])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+
+        print(userInfo)
+
+        completionHandler()
     }
 }
